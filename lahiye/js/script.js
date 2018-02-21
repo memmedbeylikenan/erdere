ca2003139cb9aaf51567694929461a0e37a2d0e8
memmedbeylikenan <34453675+memmedbeylikenan@users.noreply.github.com>
2018-02-21 21:20:51 +0400

    Add files via upload

---
+++ lahiye/js/script.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+//     $(window).scroll(function (ev) {
+//         if ($(this).scrollTop() > 100) {
+//             $('#header').addClass("scrolled");
+//         } else {
+//             $('#header').removeClass("scrolled");
+//         }
+
+//         if ($(this).scrollTop() >= $("#about").offset().top-15) {
+//             $(".nav-item.active").removeClass("active")
+//             $('[href="#about"]').parent().addClass("active");
+//         }
+//     })
+// })
